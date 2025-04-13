578da57bb2b4c1cf731b9c60f9d15f2bb077150a
bd2720 <brendandeneen9@gmail.com>
2025-04-13 15:33:36 -0400

    create usePinImage, switch between circle and symbol layers

--- src/app/components/map.tsx
+++ src/app/components/map.tsx
@@ -3,7 +3,8 @@
 import ReactMapGL, { Source, Layer, MapRef, MapMouseEvent } from 'react-map-gl/mapbox';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import { FeatureCollection } from 'geojson';
-import { useRef, useEffect, useState } from 'react';
+import { useRef, useState } from 'react';
+import { usePinImage } from '../hooks/use-pin-image';
 
 interface MapProps {
   data: FeatureCollection | null
@@ -12,39 +13,27 @@ interface MapProps {
 export default function Map({ data }: MapProps){
   const mapRef = useRef<MapRef>(null);
   const [mapLoaded, setMapLoaded] = useState(false);
-  const [imageLoaded, setImageLoaded] = useState(false);
-  const [hoveredPinId, setHoveredPinId] = useState<string | number | undefined>();
+  const [hoveredPointId, setHoveredPointId] = useState<string | number | undefined>();
+  const [selectedPointId, setSelectedPointId] = useState<string | number | undefined>();
 
-  // load map pin image
-  useEffect(() => {
-    const map = mapRef.current?.getMap();
-    if(!mapLoaded || !map) return;
+  // load pin image in custom hook
+  const imageLoaded = usePinImage(mapRef, mapLoaded);
 
-    // check if we already have image
-    if(map.hasImage('pin')){
-      setImageLoaded(true);
-      return;
-    }
-
-    // attempt to load image
-    map.loadImage('/pin.png', (error, image) => {
-      if(error || !image){
-        console.error('Failed to load pin.png', error);
-        return;
-      }
-      if (!map.hasImage('pin')){
-        map.addImage('pin', image, { sdf: true });
-        setImageLoaded(true);
-
-      }
-    });
-  }, [mapLoaded]);
+  // determine number of features (use point layer if too many)
+  const numFeatures = data?.features.length ?? 0;
 
   // called when the mouse is moving over the map
   function handleMouseMove(e: MapMouseEvent){
     if(!e.features) return;
-    // update hovered pin
-    setHoveredPinId((e.features.length) ? e.features[0].id : undefined);
+    // update hovered point
+    setHoveredPointId((e.features.length) ? e.features[0].id : undefined);
+  }
+
+  // called when the mouse clicks on the map
+  function handleMouseDown(e: MapMouseEvent){
+    if(!e.features?.length) return;
+    // update selected point
+    setSelectedPointId((e.features[0].id !== selectedPointId) ? e.features[0].id : undefined);
   }
 
   return (
@@ -58,35 +47,60 @@ export default function Map({ data }: MapProps){
           latitude: 40.68,
           zoom: 10,
         }}
-        interactiveLayerIds={["data-pin"]}
-        cursor={(hoveredPinId !== undefined) ? 'pointer' : 'auto'}
+        interactiveLayerIds={["data-pin", "data-point"]}
+        cursor={(hoveredPointId !== undefined) ? 'pointer' : 'auto'}
         onLoad={() => setMapLoaded(true)}
         onMouseMove={handleMouseMove}
+        onMouseDown={handleMouseDown}
       >
         {data && imageLoaded && (
           <Source type="geojson" data={data} generateId>
-            <Layer type="symbol" id="data-pin"
-              layout={{
-                "icon-allow-overlap": true,
-                "icon-image": "pin",
-                "icon-size": 0.69,
-                "icon-offset": [0, -25],
-                "symbol-sort-key": [
-                  "match",
-                  ["id"], 
-                  hoveredPinId ?? '', 1,
-                  0
-                ]
-              }}
-              paint={{
-                "icon-color": [
-                  "match",
-                  ["id"],
-                  hoveredPinId ?? '', "#64748b",
-                  "#020617"
-                ]
-              }}
-            />
+            {
+              (numFeatures < 100) ? (
+                <Layer type="symbol" id="data-pin"
+                  layout={{
+                    "icon-allow-overlap": true,
+                    "icon-image": "pin",
+                    "icon-size": 0.69,
+                    "icon-offset": [0, -25],
+                    "symbol-sort-key": [
+                      "case",
+                      ["==", ["id"], hoveredPointId ?? null], 2,
+                      ["==", ["id"], selectedPointId ?? null], 1,
+                      0
+                    ]
+                  }}
+                  paint={{
+                    "icon-color": [
+                      "case",
+                      ["==", ["id"], hoveredPointId ?? null], "#64748b",
+                      ["==", ["id"], selectedPointId ?? null], "#64748b",
+                      "#020617"
+                    ]
+                  }}
+                />
+              ) : (
+                <Layer type="circle" id="data-point"
+                  layout={{
+                    "circle-sort-key": [
+                      "case",
+                      ["==", ["id"], hoveredPointId ?? null], 2,
+                      ["==", ["id"], selectedPointId ?? null], 1,
+                      0
+                    ]
+                  }}
+                  paint={{
+                    "circle-radius": 5,
+                    "circle-color": [
+                      "case",
+                      ["==", ["id"], hoveredPointId ?? null], "#64748b",
+                      ["==", ["id"], selectedPointId ?? null], "#64748b",
+                      "#020617"
+                    ]                    
+                  }}
+                />
+              )
+            }  
           </Source>
         )}
       </ReactMapGL>
