82ea8e39096beb3aa51d53a438ab93050417a124
bd2720 <brendandeneen9@gmail.com>
2025-05-13 18:14:52 -0400

    fix cron job authorization

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -4,13 +4,13 @@ import { redis } from "@/app/redis/connect";
 // Geoapify's daily free limit
 const GEOAPIFY_LIMIT_DAY = process.env.GEOAPIFY_LIMIT_DAY ?? 3000;
 // auth secret
-const GEOAPIFY_CRON_SECRET = process.env.GEOAPIFY_CRON_SECRET ?? null;
+const CRON_SECRET = process.env.CRON_SECRET ?? null;
 
 /* Reset the daily API call limit */
 export async function GET(req: NextRequest){
   // check secret
-  const secret = req.headers.get('x-vercel-cron-secret');
-  if(GEOAPIFY_CRON_SECRET === null || secret !== GEOAPIFY_CRON_SECRET){
+  const secret = req.headers.get('authorization');
+  if(CRON_SECRET === null || secret !== `Bearer ${CRON_SECRET}`){
     return new Response('Unauthorized', { status: 401 });
   }
 
